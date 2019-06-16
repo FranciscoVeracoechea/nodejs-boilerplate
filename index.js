14a72c0f07e019332aa67fd12bcc0195aa16c7a3
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2019-06-16 12:28:10 -0400

    adding env settings helmet and public folder

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // dependencies
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
-// middlewares
-// import errorHandler from './middlewares/errorHandler';
-// import deviceDetection from './middlewares/deviceDetection';
+
+
+// settings
+dotenv.config();
+const isDev = process.env.NODE_ENV === 'development';
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+// error handler
+// app.use(errorHandler({ isDev }));
+
+app.listen(process.env.NODE_PORT, (err) => {
+  if (!err) {
+    /* eslint-disable */
+    console.log(`Server listening on ${process.env.APP_URL}`);
+    /* eslint-enable */
+  }
+});
